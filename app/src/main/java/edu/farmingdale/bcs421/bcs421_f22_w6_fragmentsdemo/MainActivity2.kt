0e24dbb6e4bf0b4883d7198be1b34377b5f4fa94
Elan Cheung <elan.cheung@gmail.com>
2023-10-07 03:06:01 -0400

    Added SeekBar to activity_main2.xml and changed max to 35.
    Added code for SeekBar to update EditText field with current textsize of the textview and the progress of the seekbar.

--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
@@ -3,13 +3,16 @@ package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var mBtn1:Button
     lateinit var mBtn2:Button
     lateinit var mTV:TextView
-
+    lateinit var mSB:SeekBar
+    lateinit var mET: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -17,9 +20,10 @@ class MainActivity2 : AppCompatActivity() {
         mBtn1 = findViewById(R.id.btn1);
         mBtn2 = findViewById(R.id.btn2);
         mTV = findViewById(R.id.tv1)
+        mSB = findViewById(R.id.sbChangeText)
+        mET = findViewById(R.id.editText01)
         var frgmnt01 = Fragment01()
         var frgmnt02 = Fragment02()
-
 
         mBtn1.setOnClickListener{
             supportFragmentManager.beginTransaction().apply {
@@ -34,6 +38,22 @@ class MainActivity2 : AppCompatActivity() {
             }
             readFromSharedPref()
         }
+
+        //Listener for the SeekBar to adjust the TextView's text size
+        mSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //Textsize starts off at 15 and maxes at 50 due to 35 max in xml code
+                val textSize = 15 + progress
+                //Converts textsize to float
+                mTV.textSize = textSize.toFloat()
+                //Updates EditText field with textsize
+                mET.setText(textSize.toString())
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 
     private fun readFromSharedPref(){
